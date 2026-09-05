cdd772362744ac13570d3abf6cfaaf809cffea41
MJ <tywf91@gmail.com>
2026-09-05 09:42:18 +0800

    release: v0.27.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Dala.MixProject do
   def project do
     [
       app: :dala,
-      version: "0.27.5",
+      version: "0.27.6",
       elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
